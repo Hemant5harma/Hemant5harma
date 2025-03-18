30475f7453b7b3b94b203bc150d2abcc1e0bc838
hemant5harma <hemantsharmahare@gmail.com>
2025-03-19 00:44:47 +0530

    dca and data

--- backend/core/config.py
+++ backend/core/config.py
@@ -6,7 +6,12 @@ class Settings(BaseSettings):
     INFURA_API_KEY: str
     CONTRACT_ADDRESS: str
     PRIVATE_KEY: str
-    
+    DB_HOST: str
+    DB_PORT: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+
     class Config:
         env_file = ".env"
 
